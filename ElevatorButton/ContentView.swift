ad655ea7ecd410e1505e6e6d4638344e38426522
Samuel Siburian <samuel.siburian@adroady.com>
2020-12-07 14:23:47 +0700

    Initial Commit

---
+++ ElevatorButton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ElevatorButton
+//
+//  Created by Samuel Siburian on 07/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ElevatorButton/ElevatorButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElevatorButtonApp.swift
+//  ElevatorButton
+//
+//  Created by Samuel Siburian on 07/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ElevatorButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
